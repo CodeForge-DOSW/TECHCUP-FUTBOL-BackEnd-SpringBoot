88b78d07af5182f47d0777602c7bd3fadfbb8f2c
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-04 17:12:16 -0500

    Crear archivo base

---
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/creacionales/reto2/reto2.java
@@ -0,0 +1,7 @@
+package src.main.java.edu.dosw.lab.creacionales.reto2;
+
+public class reto2 {
+    public static void main(String[] args) {
+        System.out.println("RETO #2: El chef de 5 estrellas");
+    }
+}
