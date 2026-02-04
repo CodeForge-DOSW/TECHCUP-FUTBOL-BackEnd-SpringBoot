082e3c50c779f2d2b1886920098707816bed0623
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-04 17:16:10 -0500

    Crear archivo base

---
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/creacionales/reto4/reto4.java
@@ -0,0 +1,7 @@
+package src.main.java.edu.dosw.lab.creacionales.reto4;
+
+public class reto4 {
+    public static void main(String[] args) {
+        System.out.println("RETO #4: La Estafa de la Casa de Cambio");
+    }
+}
