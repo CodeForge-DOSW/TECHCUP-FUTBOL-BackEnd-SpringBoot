5b4e3da2afe73815e1215b03cab81e534338d081
juan.cruz-r <juanestebancruzrico@gmail.com>
2026-02-04 18:08:35 -0500

    Creación de la interfaz vehicule

---
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/creacionales/Reto3/Vehicle.java
@@ -0,0 +1,8 @@
+public interface Vehicle {
+
+    public abstract String getType();
+    public abstract String getCategory();
+    public abstract int getMaxSpeed();
+    public abstract double getPrice();
+    public abstract String getEquipment();
+}
