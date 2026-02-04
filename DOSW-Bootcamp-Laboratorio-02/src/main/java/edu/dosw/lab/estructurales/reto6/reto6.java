84a0fed441d6cce1c4cf87a872dba19b4f2cfae2
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-04 18:03:34 -0500

    Crear archivo base

---
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/estructurales/reto6/reto6.java
@@ -0,0 +1,7 @@
+package src.main.java.edu.dosw.lab.estructurales.reto6;
+
+public class reto6 {
+    public static void main(String[] args) {
+        System.out.println("RETO #5: El Café Personalizado");
+    }
+}
