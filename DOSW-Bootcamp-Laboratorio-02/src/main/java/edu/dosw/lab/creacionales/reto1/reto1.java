937161773dd588cad6e793257f42e8e2ce30f400
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-04 16:56:15 -0500

    Crear archivo base

---
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/creacionales/reto1/reto1.java
@@ -0,0 +1,7 @@
+package src.main.java.edu.dosw.lab.creacionales.reto1;
+
+public class reto1 {
+    public static void main(String[] args) {
+        System.out.println("RETO #1: El problema de la tienda de Don Pepe");
+    }
+}
