63d06d87d2bf179d1a7c2a675823add951d576c4
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-10 23:54:05 -0500

    Llamado al reto

--- DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/Application.java
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/Application.java
@@ -1,7 +1,12 @@
 package edu.dosw.lab;
 
+import main.java.edu.dosw.lab.comportamiento.reto6.reto6;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
+
+        System.out.println("\nRETO #6: Habla con Soporte Técnico\n");
+        reto6.ejecutar();
     }
 }
