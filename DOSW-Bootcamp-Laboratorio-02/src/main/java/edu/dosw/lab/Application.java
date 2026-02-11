c025af19b46b1943ad321d515440f24604827448
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-11 15:18:10 -0500

    Llamado reto 1, 2, 5, 6

--- DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/Application.java
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/Application.java
@@ -1,7 +1,24 @@
 package edu.dosw.lab;
 
+import src.main.java.edu.dosw.lab.creacionales.reto1.Reto1;
+import src.main.java.edu.dosw.lab.creacionales.reto2.Reto2;
+import edu.dosw.lab.estructurales.reto5.reto5;
+import main.java.edu.dosw.lab.comportamiento.reto6.reto6;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
+
+        System.out.println("RETO #1: El problema de la tienda de Don Pepe");
+        Reto1.main(args);
+
+        System.out.println("RETO #2: El chef de 5 estrellas");
+        Reto2.main(args);
+
+        System.out.println("\nRETO #5: El Café Personalizado\n");
+        reto5.run();
+
+        System.out.println("\nRETO #6: Habla con Soporte Técnico\n");
+        reto6.ejecutar();
     }
 }
