48fc41678d5ddad3c5c9084810a768acc00a0bae
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-11 15:24:52 -0500

    Llamado reto 3, 4

--- DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/Application.java
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/edu/dosw/lab/Application.java
@@ -1,5 +1,7 @@
 package edu.dosw.lab;
 
+import edu.dosw.lab.creacionales.Reto3.Reto3;
+import edu.dosw.lab.estructurales.Reto4.Reto4;
 import src.main.java.edu.dosw.lab.creacionales.reto1.Reto1;
 import src.main.java.edu.dosw.lab.creacionales.reto2.Reto2;
 import edu.dosw.lab.estructurales.reto5.reto5;
@@ -15,14 +17,16 @@ public class Application {
         System.out.println("RETO #2: El chef de 5 estrellas");
         Reto2.main(args);
 
+        System.out.println("RETO #3: El Reino de los Vehículos");
         Reto3.main(args);
 
+        System.out.println("RETO #4: La Estafa de la Casa de Cambio");
         Reto4.main(args);
 
-        System.out.println("\nRETO #5: El Café Personalizado\n");
+        System.out.println("RETO #5: El Café Personalizado");
         reto5.run();
 
-        System.out.println("\nRETO #6: Habla con Soporte Técnico\n");
+        System.out.println("RETO #6: Habla con Soporte Técnico");
         reto6.ejecutar();
     }
 }
