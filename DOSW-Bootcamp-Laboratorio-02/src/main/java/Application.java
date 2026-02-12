25124d0cec074895aedc18a7361d66c328724fce
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-02-12 12:33:01 -0500

    Correccion llamados y path

--- DOSW-Bootcamp-Laboratorio-02/src/main/java/Application.java
+++ DOSW-Bootcamp-Laboratorio-02/src/main/java/Application.java
@@ -1,34 +1,31 @@
-import src.main.java.edu.dosw.lab.creacionales.reto1.Reto1;
-import src.main.java.edu.dosw.lab.creacionales.reto2.Reto2;
+import edu.dosw.lab.creacionales.reto1.Reto1;
+import edu.dosw.lab.creacionales.reto2.Reto2;
 import edu.dosw.lab.creacionales.Reto3.Reto3;
 import edu.dosw.lab.estructurales.Reto4.Reto4;
 import edu.dosw.lab.estructurales.reto5.reto5;
-//import main.java.edu.dosw.lab.comportamiento.reto6.reto6;
-//import edu.dosw.lab.comportamiento.reto7.Reto7;
+import edu.dosw.lab.comportamiento.reto6.reto6;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("✅ Proyecto Maven configurado y corriendo correctamente");
 
-//        System.out.println("RETO #1: El problema de la tienda de Don Pepe");
-//        Reto1.main(args);
+        System.out.println("RETO #1: El problema de la tienda de Don Pepe");
+        Reto1.main(args);
 
-//        System.out.println("RETO #2: El chef de 5 estrellas");
-//        Reto2.main(args);
+        System.out.println("RETO #2: El chef de 5 estrellas");
+        Reto2.main(args);
 
-//        System.out.println("RETO #3: El Reino de los Vehículos");
-//        Reto3.main(args);
+        System.out.println("RETO #3: El Reino de los Vehículos");
+        Reto3.main(args);
 
-//        System.out.println("RETO #4: La Estafa de la Casa de Cambio");
-//        Reto4.main(args);
+        System.out.println("RETO #4: La Estafa de la Casa de Cambio");
+        Reto4.main(args);
 
         System.out.println("RETO #5: El Café Personalizado");
         reto5.run();
-//
-//        System.out.println("RETO #6: Habla con Soporte Técnico");
-//        reto6.ejecutar();
-//
-//        System.out.println("RETO #7: El control remoto Mágico");
-//        Reto7.main(args);
+
+        System.out.println("RETO #6: Habla con Soporte Técnico");
+        reto6.ejecutar();
+
     }
 }
