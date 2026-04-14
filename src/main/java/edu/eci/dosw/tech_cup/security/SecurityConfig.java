f3c2cc1cadcfb14491185689941082c4379a8f81
brayan loaiza leal <alvarezdairo9@gmail.com>
2026-04-14 01:23:32 -0500

    fix: permitir acceso publico a Swagger UI en Spring Security

--- src/main/java/edu/eci/dosw/tech_cup/security/SecurityConfig.java
+++ src/main/java/edu/eci/dosw/tech_cup/security/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
             .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
